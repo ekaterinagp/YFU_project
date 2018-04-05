6d1c5f80649e19ee585532e5049b692cab17bca5
liyan <yanx0167@stud.kea.dk>
2018-04-05 15:12:23 +0200

    ADDED: sticky logo and navi

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.onre
+//document.onre
 
 var china = document.getElementById('china');
 console.log("china: ", china);
@@ -68,3 +68,21 @@ window.onclick = function(event) {
 //   x[slideIndex - 1].style.display = "block";
 //   dots[slideIndex - 1].className += " opacity-off";
 // }
+
+let stickyLogo = document.querySelector('.logo-black');
+let stickyNavi = document.querySelector('.ul-sticky');
+let des = document.querySelector('#destinations');
+window.addEventListener('scroll', getAndCheckNavOffsetTop);
+function getAndCheckNavOffsetTop() {
+  let offset = des.getBoundingClientRect();
+  if (offset.top <= 117) {
+    stickyLogo.style.position = "fixed";
+    stickyLogo.style.top = "-15px";
+    stickyLogo.classList.add('show');
+    stickyNavi.classList.add('show');
+    stickyNavi.style.position = "fixed";
+    stickyNavi.style.top = "6vw";
+  } else {
+    stickyNavi.classList.remove('show');
+  }
+}
